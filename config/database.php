1da921cc87f1fc6b3e5f657e810d926ffeedf018
unknown <marlon_jardim_@hotmail.com>
2018-01-23 14:24:41 -0200

    Created the models and an example view for testing purposes.

--- config/database.php
+++ config/database.php
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'homestead'),
+            'username' => env('DB_USERNAME', 'homestead'),
+            'password' => env('DB_PASSWORD', 'secret'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+
+use App\Clinic;
+use App\HealthInsuranceCompany;
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,6 +19,28 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/clinicas', function () {
+    return view('clinics');
+
+});Route::get('/planos-de-saude', function () {
+    return view('health_insurance');
+});
+Route::post('/cadastro/clinica', function (Request $request) {
+    
+    $clinic = new Clinic;
+    $clinic->nome = $request->nome;
+    $clinic->cnpj = $request->cnpj;
+    $clinic->user_id =  Auth::user()->id;
+    $clinic->save();
+
+    return redirect('/');
+    //
+})->name('clinic_signup');
+
+Route::post('/cadastro/plano-de-saude', function (Request $request) {
+    //
+})->name('health_insurance_signup');
 
 Auth::routes();
 
