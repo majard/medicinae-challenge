a525b507459ad75ec147ca74e57bc30c4aeb33ad
unknown <marlon_jardim_@hotmail.com>
2018-01-30 01:26:02 -0200

    Undone soft delete because of possible conflicts

--- app/Clinic.php
+++ app/Clinic.php
@@ -3,20 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Clinic extends Model
 {
-    use SoftDeletes;
-
-    /**
-     * The attributes that should be mutated to dates.
-     *
-     * @var array
-     */
-    protected $dates = ['deleted_at'];
-
-    protected $fillable = ['cnpj', 'nome'];
     
     public function health_insurance_companies()
     {
